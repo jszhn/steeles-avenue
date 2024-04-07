b4371a30209634ebeeffcaf15a75362238236971
Jason Zhang <zhangj1079@gmail.com>
2024-04-07 18:25:15 -0400

    start/end screen test

--- src/lib/graphics.c
+++ src/lib/graphics.c
@@ -1,7 +1,14 @@
 #include "graphics.h"
+#include "../assets/start_screen.c"
+#include "../assets/end_screen.c"
 
 PIXEL_t *const dPixelBuffer = (PIXEL_t*) PIXEL_BUF_CTRL_BASE;
 
+struct fb_t {
+    unsigned short volatile pixels[256][512];
+};
+
+struct fb_t *const fbp = ((struct fb_t *) 0x8000000);
 short int buffer[Y_MAX][X_MAX + 192]; // 240 rows, 512 columns
 
 void PlotSpriteAtColRow (const uint_8 row, const uint_8 col) {
@@ -30,7 +37,7 @@ void PlotPixel (uint_32 x, uint_32 y, short int line_colour) {
 }
 
 void ClearScreen (void) {
-    dPixelBuffer->BACK_BUFFER = (int) &buffer;
+    dPixelBuffer->BACK_BUFFER = (int) fbp;
     for (int y = 0; y < Y_MAX; y++) // iterate through each pixel
         for (int x = 0; x < X_MAX; x++)
             PlotPixel(x, y, WHITE);
@@ -44,4 +51,31 @@ void WaitForVSync (void) {
     while (status & 0x1) // polling loop waiting for S bit to go to 0
         status = dPixelBuffer->STATUS;
     // exits when refresh done
+}
+
+void DrawCanvas (const int canvas_number) {
+    uint_16** canvas;
+    switch (canvas_number) {
+        case (1):
+            canvas = start_screen;
+            break;
+        case (2):
+            canvas = end_screen;
+            break;
+        default: return; // should never happen
+    }
+
+    for (int x = 0; x < X_MAX; x++) {
+        for (int y = 0; y < Y_MAX; y++) {
+            fbp->pixels[y][x] = canvas[x][y];
+        }
+    }
+}
+
+int main (void) {
+    // gcc -save-temps -CC -S src/lib/graphics.c
+    ClearScreen();
+    DrawCanvas(1);
+    WaitForVSync();
+    for (;;);
 }
