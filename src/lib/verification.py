adf7851beaf5d63c867101e5b26eae063f0eefaa
Jason Zhang <zhangj1079@gmail.com>
2024-10-03 18:36:15 -0400

    update gitignore, remove binary files

--- src/lib/verification.py
+++
@@ -1,9 +0,0 @@
-# A file to verify C function correctness
-def val2bin(write: int) -> int:
-    bin = 1
-    for i in range(write):
-        bin = bin << 1
-        bin += 1
-    return bin
-
-print(bin(val2bin(9)))
